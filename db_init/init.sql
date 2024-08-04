372d14b94d92fe48aae02e8a4a2f9f4e34c3f16e
XION <igor97645@gmail.com>
2024-08-04 11:37:58 +0300

    added db init file

---
+++ db_init/init.sql
@@ -0,0 +1,40 @@
+
+CREATE TABLE IF NOT EXISTS doctor (
+    id BIGSERIAL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS patient (
+    id BIGSERIAL,
+    firstname VARCHAR,
+    lastname VARCHAR,
+    phone VARCHAR UNIQUE,
+    email VARCHAR UNIQUE,
+    dob DATE,
+    address VARCHAR,
+    doctor_id BIGINT,
+    security_number VARCHAR UNIQUE,
+
+    PRIMARY KEY (id),
+    FOREIGN KEY (doctor_id) REFERENCES doctor(id)
+);
+
+CREATE TABLE IF NOT EXISTS health_record (
+    id BIGSERIAL,
+    patient_id BIGINT,
+    name VARCHAR,
+    description VARCHAR,
+    start DATE,
+    finish DATE,
+    medication VARCHAR,
+
+    PRIMARY KEY (id),
+    FOREIGN KEY (patient_id) REFERENCES patient(id)
+);
+
+
+
+
+
+
+
