f6a59b961914a6ebd7c08b36467cc47a981e9ece
XION <igor97645@gmail.com>
2024-08-05 14:40:58 +0300

    updated init.sql

--- db_init/init.sql
+++ db_init/init.sql
@@ -1,36 +1,56 @@
+CREATE TABLE IF NOT EXISTS doctor
+(
+    id        BIGSERIAL,
+    firstname VARCHAR,
+    lastname  VARCHAR,
+    dob       DATE,
+    phone     VARCHAR UNIQUE,
+    email     VARCHAR UNIQUE,
+    position  VARCHAR,
 
-CREATE TABLE IF NOT EXISTS doctor (
-    id BIGSERIAL,
     PRIMARY KEY (id)
 );
 
-CREATE TABLE IF NOT EXISTS patient (
-    id BIGSERIAL,
-    firstname VARCHAR,
-    lastname VARCHAR,
-    gender VARCHAR,
-    phone VARCHAR UNIQUE,
-    email VARCHAR UNIQUE,
-    dob DATE,
-    address VARCHAR,
-    doctor_id BIGINT,
+CREATE TABLE IF NOT EXISTS schedule
+(
+    id             BIGSERIAL,
+    schedule_start DATE,
+    schedule_end   DATE,
+    doctor_id      BIGINT,
+
+    PRIMARY KEY (id),
+    FOREIGN KEY (doctor_id) REFERENCES doctor (id)
+);
+
+CREATE TABLE IF NOT EXISTS patient
+(
+    id              BIGSERIAL,
+    firstname       VARCHAR,
+    lastname        VARCHAR,
+    gender          VARCHAR,
+    phone           VARCHAR UNIQUE,
+    email           VARCHAR UNIQUE,
+    dob             DATE,
+    address         VARCHAR,
+    doctor_id       BIGINT,
     security_number VARCHAR UNIQUE,
 
     PRIMARY KEY (id),
-    FOREIGN KEY (doctor_id) REFERENCES doctor(id)
+    FOREIGN KEY (doctor_id) REFERENCES doctor (id)
 );
 
-CREATE TABLE IF NOT EXISTS health_record (
-    id BIGSERIAL,
-    patient_id BIGINT,
-    name VARCHAR,
+CREATE TABLE IF NOT EXISTS health_record
+(
+    id          BIGSERIAL,
+    patient_id  BIGINT,
+    name        VARCHAR,
     description VARCHAR,
-    start DATE,
-    finish DATE,
-    medication VARCHAR,
+    start       DATE,
+    finish      DATE,
+    medication  VARCHAR,
 
     PRIMARY KEY (id),
-    FOREIGN KEY (patient_id) REFERENCES patient(id)
+    FOREIGN KEY (patient_id) REFERENCES patient (id)
 );
 
 
